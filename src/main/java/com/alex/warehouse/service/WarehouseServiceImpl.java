9370dabdf5210dbee520021b90415d46424f2724
AlexNoutbook <informghp3@gmail.com>
2024-07-22 16:38:36 +0300

    Добавил @PutMapping("/warehouse-add-fuel")

--- src/main/java/com/alex/warehouse/service/WarehouseServiceImpl.java
+++ src/main/java/com/alex/warehouse/service/WarehouseServiceImpl.java
@@ -1,6 +1,7 @@
 package com.alex.warehouse.service;
 
-import com.alex.warehouse.dao.BaseDAO;
+import com.alex.warehouse.dao.WarehouseDAOImpl;
+import com.alex.warehouse.entity.Nomenclature;
 import com.alex.warehouse.entity.Warehouse;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,33 +11,37 @@ import java.util.List;
 
 @Service
 public class WarehouseServiceImpl implements BaseService<Warehouse>{
-    private BaseDAO<Warehouse> baseDAO;
+    private WarehouseDAOImpl warehouseDAO;
     @Autowired
-    public WarehouseServiceImpl(BaseDAO<Warehouse> baseDAO) {
-        this.baseDAO = baseDAO;
+    public WarehouseServiceImpl(WarehouseDAOImpl warehouseDAO) {
+        this.warehouseDAO = warehouseDAO;
     }
 
     @Override
     @Transactional
     public List<Warehouse> getAllEntity() {
-        return baseDAO.getAllEntity();
+        return warehouseDAO.getAllEntity();
     }
 
     @Override
     @Transactional
     public Warehouse saveEntity(Warehouse warehouse) {
-        return baseDAO.saveEntity(warehouse);
+        return warehouseDAO.saveEntity(warehouse);
     }
 
     @Override
     @Transactional
     public Warehouse getEntity(int id) {
-        return baseDAO.getEntity(id);
+        return warehouseDAO.getEntity(id);
     }
 
     @Override
     @Transactional
     public void deleteEntity(int id) {
-        baseDAO.deleteEntity(id);
+        warehouseDAO.deleteEntity(id);
+    }
+
+    public Warehouse getEntityByNomenclature(Nomenclature nomenclature){
+        return warehouseDAO.getEntityByNomenclature(nomenclature);
     }
 }
