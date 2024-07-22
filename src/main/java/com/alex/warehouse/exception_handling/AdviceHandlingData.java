df9f87549f412b4b50ce6bb372649edacfe008aa
AlexNoutbook <informghp3@gmail.com>
2024-07-22 09:11:11 +0300

    Добавил обработку ещё одного исключения EntittyNotFound

--- src/main/java/com/alex/warehouse/exception_handling/AdviceHandlingData.java
+++ src/main/java/com/alex/warehouse/exception_handling/AdviceHandlingData.java
@@ -1,5 +1,6 @@
 package com.alex.warehouse.exception_handling;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,5 +11,9 @@ public class AdviceHandlingData {
     @ExceptionHandler
     public ResponseEntity<HandlingData> incorrectData(NoSuchDataException exception){
         return new ResponseEntity<>(new HandlingData(exception.getMessage()), HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler
+    public ResponseEntity<HandlingData> incorrectData(EntityNotFoundException exception){
+        return new ResponseEntity<>(new HandlingData(exception.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
