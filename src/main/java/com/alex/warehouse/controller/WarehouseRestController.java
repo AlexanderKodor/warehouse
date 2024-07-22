dcceb6916367d2b6836c05dffdf7fa6163baffc5
AlexNoutbook <informghp3@gmail.com>
2024-07-22 16:38:39 +0300

    Добавил @PutMapping("/warehouse-add-fuel")

--- src/main/java/com/alex/warehouse/controller/WarehouseRestController.java
+++ src/main/java/com/alex/warehouse/controller/WarehouseRestController.java
@@ -3,7 +3,7 @@ package com.alex.warehouse.controller;
 import com.alex.warehouse.entity.Warehouse;
 import com.alex.warehouse.exception_handling.HandlingData;
 import com.alex.warehouse.exception_handling.NoSuchDataException;
-import com.alex.warehouse.service.BaseService;
+import com.alex.warehouse.service.WarehouseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,16 +12,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class WarehouseRestController {
-    private BaseService<Warehouse> baseService;
+    private WarehouseServiceImpl warehouseService;
 
     @Autowired
-    public WarehouseRestController(BaseService<Warehouse> baseService) {
-        this.baseService = baseService;
+    public WarehouseRestController(WarehouseServiceImpl warehouseService) {
+        this.warehouseService = warehouseService;
     }
 
     @GetMapping("/warehouse")
     public List<Warehouse> showAllEntity(){
-        List<Warehouse> warehouseList = baseService.getAllEntity();
+        List<Warehouse> warehouseList = warehouseService.getAllEntity();
         if(warehouseList.isEmpty()){
             throw new NoSuchDataException("Информация по данному запросу отсутсвует.");
         }
@@ -29,7 +29,7 @@ public class WarehouseRestController {
     }
     @GetMapping("/warehouse/search")
     public Warehouse showEntity(@RequestParam("id") int id){
-        Warehouse warehouse = baseService.getEntity(id);
+        Warehouse warehouse = warehouseService.getEntity(id);
         if(warehouse==null){
             throw new NoSuchDataException("Склад с id - " + id + " отсутствует.");
         }
@@ -38,21 +38,31 @@ public class WarehouseRestController {
 
     @PostMapping("/warehouse")
     public Warehouse saveEntity(@RequestBody Warehouse warehouse){
-        return baseService.saveEntity(warehouse);
+        return warehouseService.saveEntity(warehouse);
     }
 
     @PutMapping("/warehouse")
     public Warehouse updateEntity(@RequestBody Warehouse warehouse){
-        return baseService.saveEntity(warehouse);
+        return warehouseService.saveEntity(warehouse);
+    }
+
+    @PutMapping("/warehouse-add-fuel")
+    public Warehouse addQuantity(@RequestBody Warehouse warehouseIN){
+        if (warehouseIN.getQuantity()<=0){
+            throw new NoSuchDataException("Пополнение не возможно, т.к. количество менее или равно 0.");
+        }
+        Warehouse warehouse = warehouseService.getEntity(warehouseIN.getId());
+        warehouse.setQuantity(warehouse.getQuantity()+warehouseIN.getQuantity());
+        return warehouseService.saveEntity(warehouse);
     }
 
     @DeleteMapping("/warehouse")
     public HandlingData delete(@RequestParam("id") int id){
-        Warehouse warehouse = baseService.getEntity(id);
+        Warehouse warehouse = warehouseService.getEntity(id);
         if(warehouse==null){
             throw new NoSuchDataException("Склад с id - " + id + " отсутствует.");
         }
-        baseService.deleteEntity(id);
+        warehouseService.deleteEntity(id);
         return new HandlingData("Склад с id - " + id + " удален.");
     }
 }
